1c28f36c0a2c13b85e0a49841871e39557a3f2b3
Alexander De Michieli <ademichieli@squarespace.com>
2020-10-28 18:51:21 -0700

    saving my progress

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 print('hello world')
 print("this is the footer and this is the work I have done on this branch")
 
-print('I am working on the header')
-
-print('this is my header 2 version')
+print('revisited')
