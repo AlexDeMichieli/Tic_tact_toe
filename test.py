25b0370ec62a58175bb114b369735590ab91509c
Alexander De Michieli <ademichieli@squarespace.com>
2020-10-28 21:52:32 -0700

    merge conflict saved

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
 print('hello world')
 print("this is the footer and this is the work I have done on this branch")
 
-<<<<<<< HEAD
 print('revisited')
-=======
-
-print('I am working on the header')
-
-print('this is my header 2 version')
->>>>>>> c9adb4b86449162ca37d983acdb1fdccd8b3dd30
